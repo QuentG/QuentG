e793bc4be2332d602bbbc71670246287a436a696
Quentin Gans <contact@quentingans.fr>
2024-06-15 21:39:40 +0200

    :bug: Fix date

--- main.js
+++ main.js
@@ -61,13 +61,7 @@ const getMyStack = () => {
 }
 
 const getFormattedDate = () => {
-    const date = new Date();
-
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
-
-    return `${day}/${month}/${year}`;
+    return new Date().toLocaleDateString();
 }
 
 const main = async () => {
